db0b0bd91284333036671c4c2925ceeacfc4d74b
mugdha273 <mugdhasharma0327@gmail.com>
2021-09-11 01:54:40 +0530

    added signup API

--- team#3 - 8/backend/backend/urls.py
+++ team#3 - 8/backend/backend/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from med.views import RegisterAPI
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/register/', RegisterAPI.as_view(), name='register'),
 ]
