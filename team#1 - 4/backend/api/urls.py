6d96e8be801dbadc98c303dc62cd2a666d5ded5c
Anubhav Kushwaha <k04kushwaha@gmail.com>
2021-09-11 19:18:58 +0530

    Update urls.py

--- team#1 - 4/backend/api/urls.py
+++ team#1 - 4/backend/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 
-from .views import logout, login, doctor_registration, patient_register, ping
+from .views import logout, login, doctor_registration, patient_register, ping, me
 
 urlpatterns = [
     path('ping', ping, name='Ping'),
+    path('me', me, name='Me'),
     path('login', login, name='Login'),
     path('logout', logout, name='Logout'),
     path('doc-register', doctor_registration, name='Doc Registration'),
