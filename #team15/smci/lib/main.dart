7f0718dc99d4f25cb448e59eaa0e4bd135415f97
SRJ <Suraj@SRJ>
2021-09-12 18:14:00 +0530

    buggie

--- #team15/smci/lib/main.dart
+++ #team15/smci/lib/main.dart
@@ -38,7 +38,7 @@ void main() {
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'KWIK-EZ',
+        title: "KWIK-EZ",
         builder: (context, widget) => ResponsiveWrapper.builder(
               BouncingScrollWrapper.builder(context, widget!),
               maxWidth: 1200,
